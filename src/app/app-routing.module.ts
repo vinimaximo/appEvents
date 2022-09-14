d9ff7b07ca63c827b54bad56f24515e7901f0735
Vinicius Maximo Lima <vinimaximo033@hotmail.com>
2022-09-14 11:39:20 -0300

    14/09/2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'usuarios',
+    loadChildren: () => import('./usuarios/usuarios.module').then( m => m.UsuariosPageModule)
   }
 ];
 
---
+++ src/app/usuarios/usuarios.page.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { promise } from 'protractor';
+import { PostService } from 'src/services/post.service';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.page.html',
+  styleUrls: ['./usuarios.page.scss'],
+})
+export class UsuariosPage implements OnInit {
+nome: string = "";
+limite: number = 10;
+inicial: number = 0;
+usuarios: any = null;// define uma matriz vazia  
+constructor(
+  private service: PostService,
+  private router: Router
+) { }
+
+  ngOnInit() {
+  }
+  ionViewWillEnter(){
+    //garante que a nossa tela sempre exiba os dados atualizados
+    this.usuarios = [];
+    this.inicial = 0;
+    this.carregar();
+  }
+  addUsuario(){
+    this.router.navigate(['add-usuario']);
+  }
+  carregar() {
+    return new Promise(ret =>{
+      this.usuarios=[];
+      let dados = {
+        requisicao: "listar",
+        nome: this.nome,
+        limit: this.limite,
+        start: this.inicial
+      };
+      this.service.dadosApi(dados,'usuarios.php').subscribe(data=>{
+
+        if(data['result']=='0'){
+            this.ionViewWillEnter();
+        }else{
+          for(let usuario of data['result']){
+            this.usuarios.push(usuario[0]);
+          }
+        }
+        console.log(data['result'][0][0].id);
+
+      });
+    });
+  }//fim do metodo carregar
+
+  editar(id, nome, usuario, senha_original, nivel){
+    this.router.navigate(['add-usuario/'+id+'/'+nome+'/'+usuario+'/'+senha_original+'/'+nivel]);
+  }
+  mostrar(id, nome, usuario, nivel){
+    this.router.navigate(['mostar-usuario/'+id+'/'+nome+'/'+usuario+'/'+nivel]);
+}
+  ativar(id, ativo){
+
+}
+}
